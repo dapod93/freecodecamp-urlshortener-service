7e2b2319d2d8ed287ef27021bbb2ea160f112874
dapod93 <davidliois50@gmail.com>
2025-03-17 21:44:13 +0700

    update: files
    Signed-off-by: dapod93 <davidliois50@gmail.com>

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 require("dotenv").config();
-const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
+const dns = require("dns");
+const express = require("express");
+const { URL } = require("url");
 
-// Basic Configuration
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
-/* This project needs to parse POST bodies */
-// You should mount body-parser here
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.post("/api/shorturl", function (req, res) {
+  let url = "";
+  try {
+    url = new URL(req.body.url.trim());
+  } catch (error) {
+    return res.json({ error: "Invalid URL" });
+  }
+
+  dns.lookup(url.hostname, (error, address, family) => {
+    console.log(error, address, family);
+    if (error) {
+      res.json({ error: "Invalid Hostname" });
+    }
+  });
+
+  res.json({ greeting: req.body.url });
 });
+
+app.get("/api/shorturl/:shorturl");
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
